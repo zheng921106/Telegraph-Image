3008a145242f02b6c58d1c7746502487c974ce94
xiaodao2026 <106580473+xiaodao2026@users.noreply.github.com>
2024-04-12 09:34:43 +0800

    Update [id].js
    域名修改

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -9,7 +9,7 @@ export async function onRequest(context) {  // Contents of context object
      } = context;
      context.request
     
-    const allowedDomains = ["96660.cn", "chengyu.eu", "mcetf.com"];
+    const allowedDomains = ["96660.cn", "chengyu.eu"];
 
     // Extract the Referer header or use a placeholder if not present
     const Referer = request.headers.get('Referer') || "Referer";
